f4237a29ea3998db8a5e427f42c55be8767b1a7f
Zhang-Jianing <54052427+Zhang-Jianing@users.noreply.github.com>
2020-02-19 07:27:41 +0800

    Add files via upload

---
+++ ch3/竖式问题.cpp
@@ -0,0 +1,29 @@
+//竖式问题
+//张家宁
+//CHN,3705022005****3216
+
+#include<cstdio>
+#include<cstring>
+char s[20],buf[99];
+int main()
+{
+	int count=0;
+	scanf("%s",s);
+	for (int abc=111;abc<=999;abc++)
+		for (int de=11;de<=99;de++)
+		{
+			int x=abc*(de%10),y=abc*(de/10),z=abc*de;
+			sprintf(buf,"%d%d%d%d%d",abc,de,x,y,z);
+			int ok=1;
+			for (int i=0;i<strlen(buf);i++)
+				if (strchr(s,buf[i])==NULL)
+					ok=0;
+			if (ok)
+			{
+				printf("<%d>\n",++count);
+				printf("%5d\nX%4d\n-----\n%5d\n%4d\n-----\n%5d\n\n",abc,de,x,y,z);
+			}
+		}
+	printf("The number of solutions = %d\n",count);
+	return 0;
+} 
